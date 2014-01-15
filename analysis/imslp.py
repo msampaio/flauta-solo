08a7a2da5fd0c59e824e0cca018969c485c9ec33
Marcos da Silva Sampaio <marcos@sampaio.me>
2014-01-15 20:38:57 -0300

    Classes for Imslp info

--- analysis/imslp.py
+++ analysis/imslp.py
@@ -7,6 +7,23 @@ import json
 import _utils
 
 
+class ImslpSource(object):
+    """Class for IMSLP Source info."""
+
+    def __init__(self):
+
+        self.id = None
+
+    def __eq__(self, other):
+        return _utils.equalityComparisons(self, other)
+
+    def __ne__(self, other):
+        return _utils.equalityComparisons(self, other, True)
+
+    def __repr__(self):
+        return "<ImslpSource: {0}>".format(self.id)
+
+
 def makeURL(dic):
     initURL = 'http://imslp.org/imslpscripts/API.ISCR.php?'
     data = []
@@ -38,4 +55,37 @@ def getInfo(idNumber, retformat='json'):
     dic = makeDic(idNumber, retformat)
     url = makeURL(dic)
     string = download(url)
+
     return json.loads(string)
+
+
+def makeImslpSource(idNumber):
+    """Return an object of ImslpSource class."""
+
+
+    imslpSource = ImslpSource()
+    dic = getInfo(idNumber)['0']
+
+    setattr(imslpSource, 'parent', dic['parent'])
+
+    extvals = dic['extvals']
+    setattr(imslpSource, 'editor', extvals['Editor'])
+    setattr(imslpSource, 'publisherInformation', extvals['Publisher Information'])
+    setattr(imslpSource, 'miscNotes', extvals['Misc. Notes'])
+
+    intvals = dic['intvals']
+
+    for k in intvals.keys():
+        if k.isdigit:
+            if 'index' in intvals[k]:
+                if intvals[k]['index'] == idNumber:
+                    scoreInfo = intvals[k]
+                    setattr(imslpSource, 'description', scoreInfo['description'])
+                    setattr(imslpSource, 'uploader', scoreInfo['uploader'])
+                    setattr(imslpSource, 'id', scoreInfo['index'])
+                    setattr(imslpSource, 'timestamp', scoreInfo['timestamp'])
+                    setattr(imslpSource, 'pagecount', scoreInfo['pagecount'])
+                    setattr(imslpSource, 'rawpagecount', scoreInfo['rawpagecount'])
+                    setattr(imslpSource, 'rating', scoreInfo['rating'])
+
+    return imslpSource
