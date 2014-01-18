7450356fada2dbd3b850113a4cc9e0588a6cbf83
azevedospider <azevedo.spider@gmail.com>
2014-01-18 11:59:16 -0300

    Algoritmo de Bor redução por janela - #140

--- reducaoContorno/danilo/reductBor/reductBor.py
+++ reducaoContorno/danilo/reductBor/reductBor.py
@@ -1,14 +1,30 @@
+#UNIVERSIDADE FEDERAL DA BAHIA
+#DEPARTAMENTO DE CIENCIA DA COMPUTACAO e ESCOLA DE MUSICA - GENOS
+#PROJETO FINAL DE CURSO I - Orientadores: Marco Sampaio e Flavio Assis
+# Danilo Azevedo Santos
+
 
 import aux
 
+
 class Contour:
+	""" class create object contour by functions of the
+	algorithms"""
+
 	
 	 def __init__(self, cseq):
+	 	"""initialize class contour, its receive object
+	 	and set of input in object"""
+
 	 	self.cseq = cseq
 	 	#self.reductBor(3, self.cseq)
 
+
 	 def reductBor(self):
-	 	k, newList =0, []
+	 	"""Returns a contour segment reduced by Bor Window Algorithm.
+	 	exemple input Contour([0 3 2 1]) and return ([0 3 1], 3)"""
+
+	 	k, newList = 0, []
 	 	size = len(self.cseq)
 	 	newList.append(self.cseq[0])
 	 	for i in range(size -2):
@@ -22,12 +38,17 @@ class Contour:
 	 	self.cseq = newList[:]
 	 	return self.cseq
 
+
+
 def main():
-    entrada = raw_input("(entrada - ex: 1 2 3 4): ")
+	"""Returns the execution algorithms, the metod 
+	can be removed and replaced by automatic testing future"""
+
+    entrada = raw_input(" ")
     entrada = entrada.split(" ")
     contour = aux.convert_input(entrada)
     obj = Contour(contour)
     print " (", obj.reductBor(), " 3 )" 
       
-# # # Main program, a place that we call the functions above # # #
+"""Main program, a place that we call the functions above"""
 main()  #Final process    	 	
