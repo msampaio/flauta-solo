8f6c3db7e43e51aed133400eed48eead51d0879e
Marcos da Silva Sampaio <marcos@sampaio.me>
2014-04-04 17:57:51 -0300

    Correção no módulo analysis. Close #46 #79 #156

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -1,7 +1,2 @@
 # functions
-from core import makeCity
-from core import makeCountry
-from core import makeComposer
-
-from idcode import idCodeParser
-from idcode import idCodeMaker
+from core import makeCompleteScore
