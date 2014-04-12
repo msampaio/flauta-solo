5a8dc8991e7c34aa52061f59357f0d36c43cd410
Pedro Kroger <kroger@pedrokroger.net>
2014-04-11 23:45:53 -0300

    Add basic bootstrap template

--- flute/urls.py
+++ flute/urls.py
@@ -4,8 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'flute.views.home', name='home'),
+    url(r'^$', 'analysis.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
