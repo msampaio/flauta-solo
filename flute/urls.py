0851f99f56fb174fdfab93810893c8f17f2163f6
Pedro Kroger <kroger@pedrokroger.net>
2014-04-11 23:32:06 -0300

    Add template

--- flute/urls.py
+++ flute/urls.py
@@ -4,9 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'flute.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'analysis.views.home', name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
