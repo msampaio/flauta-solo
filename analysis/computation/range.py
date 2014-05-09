a93e50926cdfca26c3b7abb81ba87479769d68ab
Marcos da Silva Sampaio <marcos@sampaio.me>
2014-05-09 09:47:27 -0300

    Add range statistics code draft

--- analysis/computation/range.py
+++ analysis/computation/range.py
@@ -1,4 +1,12 @@
-
+import numpy
 
 def range_analysis(compositions):
-    return "1"
+    range_list = [c.music_data.ambitus for c in compositions]
+
+    range_statistics = {}
+    range_statistics['Min'] = min(range_list)
+    range_statistics['Max'] = max(range_list)
+    range_statistics['Mean'] = numpy.mean(range_list)
+    range_statistics['Standard deviation'] = numpy.std(range_list)
+
+    return range_statistics
