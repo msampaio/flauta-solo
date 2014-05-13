28b651b3cab52804dfbb6c45601df7e955c203f9
Marcos da Silva Sampaio <marcos@sampaio.me>
2014-05-13 16:07:52 -0300

    Adjust chart to use real range data

--- analysis/computation/range.py
+++ analysis/computation/range.py
@@ -11,47 +11,12 @@ def range_analysis(compositions):
             frequency[v] = 0
         frequency[v] +=1
 
+    values = []
+    for k, v in sorted(frequency.items()):
+        values.append({"label": k, "value": v})
 
-    # teste para gerar gráfico.
-    r = [
-    {
-        "key": "Cumulative Return",
-        "values": [
-            {
-                "label" : "A" ,
-                "value" : 29.765957771107
-            } ,
-            {
-                "label" : "B" ,
-                "value" : 0
-            } ,
-            {
-                "label" : "C" ,
-                "value" : 32.807804682612
-            } ,
-            {
-                "label" : "D" ,
-                "value" : 196.45946739256
-            } ,
-            {
-                "label" : "E" ,
-                "value" : 0.19434030906893
-            } ,
-            {
-                "label" : "F" ,
-                "value" : 98.079782601442
-            } ,
-            {
-                "label" : "G" ,
-                "value" : 13.925743130903
-            } ,
-            {
-                "label" : "H" ,
-                "value" : 5.1387322875705
-            }
-        ]
-    }
-    ]
+    r = [{"key": "Cumulative Return",
+          "values": values}]
 
     return json.dumps(r)
     # range_statistics = {}
