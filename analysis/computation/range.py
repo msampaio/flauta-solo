2e3da7b1260e14296f54b8daf588f2afa5d19778
Marcos da Silva Sampaio <marcos@sampaio.me>
2014-05-14 11:11:29 -0300

    Split range function in specific histogram and basics functions

--- analysis/computation/range.py
+++ analysis/computation/range.py
@@ -1,18 +1,27 @@
 from collections import Counter
 import numpy
 
+def get_ambitus_list(compositions):
+    return [c.music_data.ambitus for c in compositions]
 
-def range_analysis(compositions):
-    range_list = [c.music_data.ambitus for c in compositions]
+def range_histogram(range_list):
     frequency = Counter(range_list)
     values = [{"label": k, "value": v} for k, v in sorted(frequency.items())]
-
     return [{"key": "Cumulative Return", "values": values}]
 
-    # range_statistics = {}
-    # range_statistics['Min'] = min(range_list)
-    # range_statistics['Max'] = max(range_list)
-    # range_statistics['Mean'] = numpy.mean(range_list)
-    # range_statistics['Standard deviation'] = numpy.std(range_list)
-    #
-    # return range_statistics
+def range_basics(range_list):
+    data = {}
+    data['Min'] = min(range_list)
+    data['Max'] = max(range_list)
+    data['Mean'] = numpy.mean(range_list)
+    data['Standard deviation'] = numpy.std(range_list)
+    return data
+
+def range_analysis(compositions):
+    range_list = get_ambitus_list(compositions)
+
+    data = {}
+    data['histogram'] = range_histogram(range_list)
+    data['statistics'] = range_basics(range_list)
+
+    return data
