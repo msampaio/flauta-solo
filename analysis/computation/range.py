5eb1a9964a0bca647635ab2a589f426215ce4f13
Marcos da Silva Sampaio <marcos@sampaio.me>
2014-05-14 13:08:42 -0300

    Fix range chart type

--- analysis/computation/range.py
+++ analysis/computation/range.py
@@ -4,7 +4,7 @@ import numpy
 def get_ambitus_list(compositions):
     return [c.music_data.ambitus for c in compositions]
 
-def range_histogram(range_list):
+def range_values(range_list):
     frequency = Counter(range_list)
     values = [{"label": k, "value": v} for k, v in sorted(frequency.items())]
     return [{"key": "Cumulative Return", "values": values}]
@@ -24,7 +24,7 @@ def range_analysis(compositions):
     range_list = get_ambitus_list(compositions)
 
     data = {}
-    data['histogram'] = range_histogram(range_list)
+    data['values'] = range_values(range_list)
     data['statistics'] = range_basics(range_list)
 
     return data
