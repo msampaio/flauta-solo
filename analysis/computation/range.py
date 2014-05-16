764ad443a7d5201159f1bb36a9310a9b922499cf
Pedro Kroger <kroger@pedrokroger.net>
2014-05-15 22:21:00 -0300

    Cleanup

--- analysis/computation/range.py
+++ analysis/computation/range.py
@@ -1,6 +1,7 @@
 from collections import Counter
 import numpy
 
+
 def get_ambitus_list(compositions):
     return [c.music_data.ambitus for c in compositions]
 
@@ -9,30 +10,33 @@ def range_values(range_list):
     values = [{"label": k, "value": v} for k, v in sorted(frequency.items())]
     return [{"key": "Cumulative Return", "values": values}]
 
+
 def range_histogram(range_list):
-    bins = 10;
+    bins = 10
     histogram = numpy.histogram(range_list, bins)
     values = [{"label": histogram[1][i], "value": histogram[0][i]} for i in range(bins)]
 
     return [{"key": "Cumulative Return", "values": values}]
 
+
 def range_basics(range_list):
-    data = {}
-    data['Min'] = min(range_list)
-    data['Max'] = max(range_list)
-    data['Mean'] = numpy.mean(range_list)
-    data['Median'] = numpy.median(range_list)
-    data['Quartile 1'] = numpy.percentile(range_list, 25)
-    data['Quartile 3'] = numpy.percentile(range_list, 75)
-    data['Standard deviation'] = numpy.std(range_list)
+    data = {'Min': min(range_list),
+            'Max': max(range_list),
+            'Mean': numpy.mean(range_list),
+            'Median': numpy.median(range_list),
+            'Quartile 1': numpy.percentile(range_list, 25),
+            'Quartile 3': numpy.percentile(range_list, 75),
+            'Standard deviation': numpy.std(range_list)
+    }
     return data
+
 
 def range_analysis(compositions):
     range_list = get_ambitus_list(compositions)
 
-    data = {}
-    data['values'] = range_values(range_list)
-    data['histogram'] = range_histogram(range_list)
-    data['statistics'] = range_basics(range_list)
+    data = {'values': range_values(range_list),
+            'histogram': range_histogram(range_list),
+            'statistics': range_basics(range_list)
+    }
 
     return data
