eef85a7ebfe9778401173f0e2018fa0e3a17ff1f
Marcos da Silva Sampaio <marcos@sampaio.me>
2014-06-22 11:49:05 -0300

    Use google chart instead of NVD3 and simplify code

--- analysis/computation/range.py
+++ analysis/computation/range.py
@@ -2,41 +2,26 @@ from collections import Counter
 import numpy
 
 
-def gaussian(x, mu, sig):
-    return numpy.exp(-numpy.power(x - mu, 2.) / 2 * numpy.power(sig, 2.))
-
-
 def get_ambitus_list(compositions):
     return [c.music_data.ambitus for c in compositions]
 
 
-def histogram(range_list, listOutput=True):
+def histogram(range_list):
     bins = 10
     histogram = numpy.histogram(range_list, bins)
-    if listOutput:
-        return [[histogram[1][i], histogram[0][i]] for i in range(bins)]
+    r = [['Range', 'Pieces']]
+    for i in range(bins):
+        r.append([str(histogram[1][i]), histogram[0][i]])
+    return r
 
-    values = [{"label": histogram[1][i], "value": histogram[0][i]} for i in range(bins)]
 
-    return [{"key": "Cumulative Return", "values": values}]
-
-def frequency(range_list, listOutput=True):
+def frequency(range_list):
     frequency = Counter(range_list)
 
-    if listOutput:
-        return [[k, v] for k, v in sorted(frequency.items())]
-
-    values = [{"label": k, "value": v} for k, v in sorted(frequency.items())]
-    return [{"key": "Cumulative Return", "values": values}]
-
-
-def distribution(basic_stats_dic):
-    print(basic_stats_dic['Max'])
-    x = numpy.linspace(-3, 3, basic_stats_dic['Max'])
-    mu = -1
-    sig = 1
-    curve = enumerate(gaussian(x, mu, sig))
-    return [[x, y] for x, y in curve]
+    r = [['Range', 'Pieces']]
+    for k, v in sorted(frequency.items()):
+        r.append([k, v])
+    return r
 
 
 def basic_stats(range_list):
@@ -58,10 +43,8 @@ def analysis(compositions):
 
     args = {
         'basic_stats': basic_stats_dic,
-        'frequency': frequency(range_list, False),
-        'histogram': histogram(range_list, False),
-        'histogram_list': histogram(range_list, True),
-        'distribution': distribution(basic_stats_dic),
+        'frequency': frequency(range_list),
+        'histogram': histogram(range_list),
     }
 
     return args
