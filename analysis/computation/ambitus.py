e10b8f895f32de3125c7a8442f0ed3268d13f580
Marcos da Silva Sampaio <marcos@sampaio.me>
2014-06-28 11:19:26 -0300

    Move generic functions to utils module

--- analysis/computation/ambitus.py
+++ analysis/computation/ambitus.py
@@ -1,18 +1,10 @@
 from collections import Counter
 import numpy
+from analysis.computation import utils
 
 
 def get_ambitus_list(compositions):
     return [c.music_data.ambitus for c in compositions]
-
-
-def histogram(ambitus_list):
-    bins = 10
-    histogram = numpy.histogram(ambitus_list, bins)
-    r = [['Ambitus', 'Pieces']]
-    for i in range(bins):
-        r.append([str(histogram[1][i]), histogram[0][i]])
-    return r
 
 
 def frequency(ambitus_list):
@@ -37,22 +29,13 @@ def basic_stats(ambitus_list):
     return data
 
 
-def normal_distribution(x, mu, sigma):
-    first = 1 / (sigma * numpy.sqrt(2 * numpy.pi))
-    second = -1/2 * (((x - mu) / sigma) ** 2)
-
-    return first * (numpy.e ** second)
-
-def normalization(value, mu, sigma):
-    return (value - mu) / sigma
-
 def distribution(ambitus_list):
 
     basic_data = basic_stats(ambitus_list)
     mu = basic_data['Mean']
     sigma = basic_data['Standard deviation']
 
-    normalized = [normalization(value, mu, sigma) for value in ambitus_list]
+    normalized = [utils.normalization(value, mu, sigma) for value in ambitus_list]
 
     bins = 10
     histogram = numpy.histogram(normalized, bins)
@@ -63,18 +46,10 @@ def distribution(ambitus_list):
     values = zip(histogram[0], histogram[1])
 
     for v, k in values:
-        r.append([k, v / total, v/total, normal_distribution(k, 0, 1)])
+        r.append([k, v / total, v/total, utils.normal_distribution(k, 0, 1)])
 
     return r
 
-def boxplot(ambitus_list):
-    basic_data = basic_stats(ambitus_list)
-    minimum = basic_data['Min']
-    maximum = basic_data['Max']
-    quartile_1 = basic_data['Quartile 1']
-    quartile_3 = basic_data['Quartile 3']
-
-    return ['', maximum, quartile_3, quartile_1, minimum]
 
 def analysis(compositions):
 
@@ -85,9 +60,9 @@ def analysis(compositions):
     args = {
         'basic_stats': basic_stats_dic,
         'frequency': frequency(ambitus_list),
-        'histogram': histogram(ambitus_list),
+        'histogram': utils.histogram(ambitus_list, 10, ['Ambitus', 'Pieces'], True),
         'distribution': distribution(ambitus_list),
-        'boxplot': boxplot(ambitus_list),
+        'boxplot': utils.boxplot(basic_stats_dic),
     }
 
     return args
---
+++ analysis/computation/utils.py
@@ -0,0 +1,53 @@
+import numpy
+
+
+def flatten(seq):
+    return [el for l in seq for el in l]
+
+
+def normal_distribution(x, mu, sigma):
+    first = 1 / (sigma * numpy.sqrt(2 * numpy.pi))
+    second = -1/2 * (((x - mu) / sigma) ** 2)
+
+    return first * (numpy.e ** second)
+
+
+def normalization(value, mu, sigma):
+    return (value - mu) / sigma
+
+
+def normalize_array(array, column=0):
+    c_array = array[:,column]
+    mean = c_array.mean()
+    std = c_array.std()
+    if std != 0:
+        array[:, column] = (array[:, column] - mean) / std
+
+    return array
+
+
+# chart functions #
+
+def histogram(int_sequence, bins, label, string=True):
+    hist_data = numpy.histogram(int_sequence, bins)
+    r = [label]
+    for i in range(bins):
+        if string:
+            r.append([str(hist_data[1][i]), hist_data[0][i]])
+        else:
+            r.append([hist_data[1][i], hist_data[0][i]])
+
+    return r
+
+
+def boxplot(basic_data):
+    minimum = basic_data['Min']
+    maximum = basic_data['Max']
+    quartile_1 = basic_data['Quartile 1']
+    quartile_3 = basic_data['Quartile 3']
+
+    return ['', maximum, quartile_3, quartile_1, minimum]
+
+
+def aux_pie_chart(counted_dic):
+    return sorted(([str(k), v] for k, v in counted_dic.items()), key = lambda x: x[1], reverse=True)
