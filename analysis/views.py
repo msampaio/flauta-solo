7c48355d5bb7f500e7b6208320718cf573d103af
Pedro Kroger <kroger@pedrokroger.net>
2014-07-10 12:29:40 -0300

    remove files

--- analysis/views.py
+++ analysis/views.py
@@ -1,46 +1,11 @@
-from io import StringIO, BytesIO
-import zipfile
-from django.contrib.auth import authenticate, login
-from django.http import HttpResponseRedirect, StreamingHttpResponse, HttpResponse
 from django.shortcuts import render
+
 from analysis.models import MusicData, Composition
-from analysis.computation import ambitus
-from analysis.computation import intervals
-from analysis.computation import durations
-from analysis.computation import contour
-from analysis.computation import pure_data
 from analysis.computation import cluster
 
 
 def home(request):
     return render(request, "index.html")
-
-
-def login_user(request):
-    if request.POST:
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return HttpResponseRedirect('/')
-            else:
-                return render(request, "login_user.html", {'error': True})
-        else:
-            return render(request, "login_user.html", {'error': True})
-
-    else:
-        return render(request, 'login_user.html')
-
-
-def dashboard(request):
-    music_data_count = MusicData.objects.count()
-    composition_count = Composition.objects.count()
-    args = {'music_data_count': music_data_count,
-            'composition_count': composition_count}
-    return render(request, "dashboard.html", args)
 
 
 def uniq_items_in_model(item, model=MusicData):
@@ -51,148 +16,6 @@ def uniq_items_in_model(item, model=MusicData):
 def select_filter(name, item, arguments, template='music_data__%s'):
     if item != "all":
         arguments[template % name] = item
-
-
-def show_ambitus(request):
-
-
-    if request.method == 'POST':
-        kwargs = {}
-
-        title = request.POST['select-composition']
-        key = request.POST['select-key']
-        total_duration = request.POST['select-duration']
-        time_signature = request.POST['select-time-signature']
-
-        select_filter('title__iexact', title, kwargs, template='%s')
-        select_filter('key', key, kwargs)
-        select_filter('total_duration', total_duration, kwargs)
-        select_filter('time_signature', time_signature, kwargs)
-
-        compositions = Composition.objects.filter(**kwargs)
-        args = ambitus.analysis(compositions)
-        return render(request, 'ambitus_result.html', args)
-
-    args = {'compositions': uniq_items_in_model('title', Composition),
-            'keys': uniq_items_in_model('key'),
-            'durations': uniq_items_in_model('total_duration'),
-            'signatures': uniq_items_in_model('time_signature'),
-    }
-    return render(request, 'ambitus.html', args)
-
-
-def show_intervals(request):
-    if request.method == 'POST':
-        kwargs = {}
-
-        title = request.POST['select-composition']
-        key = request.POST['select-key']
-        total_duration = request.POST['select-duration']
-        time_signature = request.POST['select-time-signature']
-
-        select_filter('title__iexact', title, kwargs, template='%s')
-        select_filter('key', key, kwargs)
-        select_filter('total_duration', total_duration, kwargs)
-        select_filter('time_signature', time_signature, kwargs)
-
-        compositions = Composition.objects.filter(**kwargs)
-        args = intervals.analysis(compositions)
-        return render(request, 'intervals_result.html', args)
-
-    args = {'compositions': uniq_items_in_model('title', Composition),
-            'keys': uniq_items_in_model('key'),
-            'durations': uniq_items_in_model('total_duration'),
-            'signatures': uniq_items_in_model('time_signature'),
-            }
-
-    return render(request, 'intervals.html', args)
-
-def show_durations(request):
-    if request.method == 'POST':
-        kwargs = {}
-
-        title = request.POST['select-composition']
-        key = request.POST['select-key']
-        time_signature = request.POST['select-time-signature']
-
-        select_filter('title__iexact', title, kwargs, template='%s')
-        select_filter('key', key, kwargs)
-        select_filter('time_signature', time_signature, kwargs)
-
-        compositions = Composition.objects.filter(**kwargs)
-        args = durations.analysis(compositions)
-        return render(request, 'durations_result.html', args)
-
-    args = {'compositions': uniq_items_in_model('title', Composition),
-            'keys': uniq_items_in_model('key'),
-            'signatures': uniq_items_in_model('time_signature'),
-            }
-
-    return render(request, 'durations.html', args)
-
-def show_contour(request):
-    if request.method == 'POST':
-        kwargs = {}
-
-        title = request.POST['select-composition']
-        key = request.POST['select-key']
-        total_duration = request.POST['select-duration']
-        time_signature = request.POST['select-time-signature']
-
-        select_filter('title__iexact', title, kwargs, template='%s')
-        select_filter('key', key, kwargs)
-        select_filter('total_duration', total_duration, kwargs)
-        select_filter('time_signature', time_signature, kwargs)
-
-        compositions = Composition.objects.filter(**kwargs)
-        args = contour.analysis(compositions)
-        return render(request, 'contour_result.html', args)
-
-    args = {'compositions': uniq_items_in_model('title', Composition),
-            'keys': uniq_items_in_model('key'),
-            'durations': uniq_items_in_model('total_duration'),
-            'signatures': uniq_items_in_model('time_signature'),
-    }
-    return render(request, 'contour.html', args)
-
-
-def show_pure_data(request):
-    if request.method == 'POST':
-        kwargs = {}
-
-        title = request.POST['select-composition']
-        key = request.POST['select-key']
-        total_duration = request.POST['select-duration']
-        time_signature = request.POST['select-time-signature']
-        markov_order = request.POST['select-markov-order']
-
-        select_filter('title__iexact', title, kwargs, template='%s')
-        select_filter('key', key, kwargs)
-        select_filter('total_duration', total_duration, kwargs)
-        select_filter('time_signature', time_signature, kwargs)
-
-        compositions = Composition.objects.filter(**kwargs)
-        args = pure_data.analysis(compositions, order=int(markov_order))
-
-        buff = BytesIO()
-        zip_archive = zipfile.ZipFile(buff, mode='w')
-
-        for key, value in args.items():
-            zip_archive.writestr(key + '.coll', "".join(value))
-
-        zip_archive.close()
-
-        response = HttpResponse(buff.getvalue(), mimetype="application/x-zip-compressed")
-        response['Content-Disposition'] = 'attachment; filename=%s' % "markov-chains.zip"
-        return response
-
-    args = {'compositions': uniq_items_in_model('title', Composition),
-            'keys': uniq_items_in_model('key'),
-            'durations': uniq_items_in_model('total_duration'),
-            'signatures': uniq_items_in_model('time_signature'),
-            'order_numbers': range(1, 11)
-            }
-    return render(request, 'pure_data.html', args)
 
 
 def show_cluster(request):
@@ -219,11 +42,3 @@ def show_cluster(request):
             'signatures': uniq_items_in_model('time_signature'),
     }
     return render(request, 'cluster.html', args)
-
-
-def stats(request):
-    args = {
-        'number_music_data': MusicData.objects.count(),
-        'number_compositions': Composition.objects.count(),
-    }
-    return render(request, 'stats.html', args)
--- flute/urls.py
+++ flute/urls.py
@@ -5,14 +5,5 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'analysis.views.home', name='home'),
-    url(r'^login/$', 'analysis.views.login_user', name='login_user'),
-    url(r'^ambitus/$', 'analysis.views.show_ambitus', name='ambitus'),
-    url(r'^intervals/$', 'analysis.views.show_intervals', name='intervals'),
-    url(r'^durations/$', 'analysis.views.show_durations', name='durations'),
-    url(r'^contour/$', 'analysis.views.show_contour', name='contour'),
-    url(r'^pure_data/$', 'analysis.views.show_pure_data', name='pure_data'),
     url(r'^cluster/$', 'analysis.views.show_cluster', name='cluster'),
-    url(r'^dashboard/$', 'analysis.views.dashboard', name='dashboard'),
-    url(r'^stats/$', 'analysis.views.stats', name='stats'),
-    url(r'^admin/', include(admin.site.urls)),
 )
