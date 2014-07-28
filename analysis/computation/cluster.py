da2ec234ce6cb7a168ade81a6cc3405936f5214d
Marcos da Silva Sampaio <marcos@sampaio.me>
2014-07-28 15:43:21 -0300

    Add clusters table

--- analysis/computation/cluster.py
+++ analysis/computation/cluster.py
@@ -32,24 +32,54 @@ def duration_ambitus_cluster(duration_ambitus):
     return utils.make_optics_plot_data(array)
 
 
-def duration_reachability(duration_ambitus):
+def duration_reachability(array):
     min_pts = 10
 
-    if len(duration_ambitus) < 10:
+    if len(array) < 10:
         min_pts = 0
 
-    array = numpy.array(duration_ambitus[1:])
     reachability_plot = utils.make_reachability_plot_data(array, min_pts)
 
     reachability_plot.insert(0, ['Piece', 'Reachability value'])
     return reachability_plot
 
 
+def make_clusters(compositions, array):
+    leaves = utils.get_optics_data(array)[-1]
+
+    clusters = []
+
+    for leave_number, leave in enumerate(leaves):
+        l_dic = {}
+        l_dic['number'] = leave_number
+        l_dic['size'] = len(leave.order)
+        songs = []
+        for n in leave.order:
+            composition = compositions.get(id=n)
+            title = composition.title
+            code = composition.music_data.score.code
+            songs.append({'title': title, 'code': code, 'first': False})
+
+        songs[0]['first'] = True
+        l_dic['songs'] = songs
+
+
+        clusters.append(l_dic)
+
+    return clusters
+
+
+
+
 def analysis(compositions):
     duration_ambitus_label = get_duration_ambitus(compositions, True, True)
     duration_ambitus = get_duration_ambitus(compositions, True, False)
     cluster = duration_ambitus_cluster(duration_ambitus)
-    reachability_plot = duration_reachability(duration_ambitus)
+
+    array = numpy.array(duration_ambitus[1:])
+
+    reachability_plot = duration_reachability(array)
+    cluster_table = make_clusters(compositions, array)
 
     if duration_ambitus:
         args = {
@@ -57,6 +87,7 @@ def analysis(compositions):
             'duration_ambitus': duration_ambitus,
             'cluster': cluster,
             'reachability_plot': reachability_plot,
+            'cluster_table': cluster_table,
         }
     else:
         args = {}
