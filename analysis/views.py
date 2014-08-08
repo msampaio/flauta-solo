0f38f82073324e9b453bdf4daf5a643a5d2b9261
Marcos da Silva Sampaio <marcos@sampaio.me>
2014-08-08 15:14:28 -0300

    Abstract compositions filters in functions

--- analysis/views.py
+++ analysis/views.py
@@ -58,66 +58,58 @@ def select_filter(name, item, arguments, template='music_data__%s'):
         arguments[template % name] = item
 
 
-def show_ambitus(request):
+def filter_compositions(request):
+    kwargs = {}
 
-    if request.method == 'POST':
-        kwargs = {}
+    title = request.POST['select-composition']
+    key = request.POST['select-key']
+    total_duration = request.POST['select-duration']
+    time_signature = request.POST['select-time-signature']
 
-        title = request.POST['select-composition']
-        key = request.POST['select-key']
-        total_duration = request.POST['select-duration']
-        time_signature = request.POST['select-time-signature']
+    select_filter('title__iexact', title, kwargs, template='%s')
+    select_filter('key', key, kwargs)
+    select_filter('total_duration', total_duration, kwargs)
+    select_filter('time_signature', time_signature, kwargs)
 
-        select_filter('title__iexact', title, kwargs, template='%s')
-        select_filter('key', key, kwargs)
-        select_filter('total_duration', total_duration, kwargs)
-        select_filter('time_signature', time_signature, kwargs)
+    compositions = Composition.objects.filter(**kwargs)
 
-        compositions = Composition.objects.filter(**kwargs)
-        args = ambitus.analysis(compositions)
+    args = {
+        'input_size': len(compositions),
+        'filter_collection': title,
+        'filter_key': key,
+        'filter_total_duration': total_duration,
+        'filter_time_signature': time_signature,
+        }
 
-        args.update({
-            'input_size': len(compositions),
-            'filter_collection': title,
-            'filter_key': key,
-            'filter_total_duration': total_duration,
-            'filter_time_signature': time_signature,
-            })
+    return compositions, args
 
-        return render(request, 'ambitus_result.html', args)
 
-    args = {'compositions': uniq_items_in_model('title', Composition),
+def make_filter_args(composition_model):
+    args = {'compositions': uniq_items_in_model('title', composition_model),
             'keys': uniq_items_in_model('key'),
             'durations': uniq_items_in_model('total_duration'),
             'signatures': uniq_items_in_model('time_signature'),
     }
+    return args
+
+
+def show_ambitus(request):
+    if request.method == 'POST':
+        compositions, args = filter_compositions(request)
+        args.update(ambitus.analysis(compositions))
+        return render(request, 'ambitus_result.html', args)
+
+    args = make_filter_args(Composition)
     return render(request, 'ambitus.html', args)
 
 
 def show_intervals(request):
     if request.method == 'POST':
-        kwargs = {}
-
-        title = request.POST['select-composition']
-        key = request.POST['select-key']
-        total_duration = request.POST['select-duration']
-        time_signature = request.POST['select-time-signature']
-
-        select_filter('title__iexact', title, kwargs, template='%s')
-        select_filter('key', key, kwargs)
-        select_filter('total_duration', total_duration, kwargs)
-        select_filter('time_signature', time_signature, kwargs)
-
-        compositions = Composition.objects.filter(**kwargs)
-        args = intervals.analysis(compositions)
+        compositions, args = filter_compositions(request)
+        args.update(intervals.analysis(compositions))
         return render(request, 'intervals_result.html', args)
 
-    args = {'compositions': uniq_items_in_model('title', Composition),
-            'keys': uniq_items_in_model('key'),
-            'durations': uniq_items_in_model('total_duration'),
-            'signatures': uniq_items_in_model('time_signature'),
-            }
-
+    args = make_filter_args(Composition)
     return render(request, 'intervals.html', args)
 
 
@@ -162,75 +154,36 @@ def composition_cluster(request, code):
 
 def show_durations(request):
     if request.method == 'POST':
-        kwargs = {}
-
-        title = request.POST['select-composition']
-        key = request.POST['select-key']
-        time_signature = request.POST['select-time-signature']
-
-        select_filter('title__iexact', title, kwargs, template='%s')
-        select_filter('key', key, kwargs)
-        select_filter('time_signature', time_signature, kwargs)
-
-        compositions = Composition.objects.filter(**kwargs)
-        args = durations.analysis(compositions)
+        compositions, args = filter_compositions(request)
+        args.update(durations.analysis(compositions))
         return render(request, 'durations_result.html', args)
 
-    args = {'compositions': uniq_items_in_model('title', Composition),
-            'keys': uniq_items_in_model('key'),
-            'signatures': uniq_items_in_model('time_signature'),
-            }
-
+    args = make_filter_args(Composition)
     return render(request, 'durations.html', args)
+
 
 def show_contour(request):
     if request.method == 'POST':
-        kwargs = {}
-
-        title = request.POST['select-composition']
-        key = request.POST['select-key']
-        total_duration = request.POST['select-duration']
-        time_signature = request.POST['select-time-signature']
-
-        select_filter('title__iexact', title, kwargs, template='%s')
-        select_filter('key', key, kwargs)
-        select_filter('total_duration', total_duration, kwargs)
-        select_filter('time_signature', time_signature, kwargs)
-
-        compositions = Composition.objects.filter(**kwargs)
-        args = contour.analysis(compositions)
+        compositions, args = filter_compositions(request)
+        args.update(contour.analysis(compositions))
         return render(request, 'contour_result.html', args)
 
-    args = {'compositions': uniq_items_in_model('title', Composition),
-            'keys': uniq_items_in_model('key'),
-            'durations': uniq_items_in_model('total_duration'),
-            'signatures': uniq_items_in_model('time_signature'),
-    }
+    args = make_filter_args(Composition)
     return render(request, 'contour.html', args)
 
 
 def show_pure_data(request):
     if request.method == 'POST':
-        kwargs = {}
-
-        title = request.POST['select-composition']
-        key = request.POST['select-key']
-        total_duration = request.POST['select-duration']
-        time_signature = request.POST['select-time-signature']
         markov_order = request.POST['select-markov-order']
 
-        select_filter('title__iexact', title, kwargs, template='%s')
-        select_filter('key', key, kwargs)
-        select_filter('total_duration', total_duration, kwargs)
-        select_filter('time_signature', time_signature, kwargs)
-
-        compositions = Composition.objects.filter(**kwargs)
-        args = pure_data.analysis(compositions, order=int(markov_order))
+        compositions, args = filter_compositions(request)
+        pure_data_args = pure_data.analysis(compositions, order=int(markov_order))
+        args.update(pure_data_args)
 
         buff = BytesIO()
         zip_archive = zipfile.ZipFile(buff, mode='w')
 
-        for key, value in args.items():
+        for key, value in pure_data_args.items():
             zip_archive.writestr(key + '.coll', "".join(value))
 
         zip_archive.close()
@@ -239,147 +192,64 @@ def show_pure_data(request):
         response['Content-Disposition'] = 'attachment; filename=%s' % "markov-chains.zip"
         return response
 
-    args = {'compositions': uniq_items_in_model('title', Composition),
-            'keys': uniq_items_in_model('key'),
-            'durations': uniq_items_in_model('total_duration'),
-            'signatures': uniq_items_in_model('time_signature'),
-            'order_numbers': range(1, 11)
-            }
+    args = make_filter_args(Composition)
+    args['order_numbers'] = range(1, 11)
+
     return render(request, 'pure_data.html', args)
 
 
 def show_cluster_duration_ambitus(request):
     if request.method == 'POST':
-        kwargs = {}
-
-        title = request.POST['select-composition']
-        key = request.POST['select-key']
-        total_duration = request.POST['select-duration']
-        time_signature = request.POST['select-time-signature']
-
-        select_filter('title__iexact', title, kwargs, template='%s')
-        select_filter('key', key, kwargs)
-        select_filter('total_duration', total_duration, kwargs)
-        select_filter('time_signature', time_signature, kwargs)
-
-        compositions = Composition.objects.filter(**kwargs)
-        args = cluster_duration_ambitus.analysis(compositions)
+        compositions, args = filter_compositions(request)
+        args.update(cluster_duration_ambitus.analysis(compositions))
         return render(request, 'cluster_duration_ambitus_result.html', args)
 
-    args = {'compositions': uniq_items_in_model('title', Composition),
-            'keys': uniq_items_in_model('key'),
-            'durations': uniq_items_in_model('total_duration'),
-            'signatures': uniq_items_in_model('time_signature'),
-    }
+    args = make_filter_args(Composition)
     return render(request, 'cluster_duration_ambitus.html', args)
 
 
 def show_cluster_intervals_frequency(request):
     if request.method == 'POST':
-        kwargs = {}
-
-        title = request.POST['select-composition']
-        key = request.POST['select-key']
-        total_duration = request.POST['select-duration']
-        time_signature = request.POST['select-time-signature']
-
-        select_filter('title__iexact', title, kwargs, template='%s')
-        select_filter('key', key, kwargs)
-        select_filter('total_duration', total_duration, kwargs)
-        select_filter('time_signature', time_signature, kwargs)
-
-        compositions = Composition.objects.filter(**kwargs)
-        args = cluster_intervals_frequency.analysis(compositions)
+        compositions, args = filter_compositions(request)
+        args.update(cluster_intervals_frequency.analysis(compositions))
         return render(request, 'cluster_intervals_frequency_result.html', args)
 
-    args = {'compositions': uniq_items_in_model('title', Composition),
-            'keys': uniq_items_in_model('key'),
-            'durations': uniq_items_in_model('total_duration'),
-            'signatures': uniq_items_in_model('time_signature'),
-    }
+    args = make_filter_args(Composition)
     return render(request, 'cluster_intervals_frequency.html', args)
 
 
 def show_cluster_durations_frequency(request):
     if request.method == 'POST':
-        kwargs = {}
-
-        title = request.POST['select-composition']
-        key = request.POST['select-key']
-        total_duration = request.POST['select-duration']
-        time_signature = request.POST['select-time-signature']
-
-        select_filter('title__iexact', title, kwargs, template='%s')
-        select_filter('key', key, kwargs)
-        select_filter('total_duration', total_duration, kwargs)
-        select_filter('time_signature', time_signature, kwargs)
-
-        compositions = Composition.objects.filter(**kwargs)
-        args = cluster_durations_frequency.analysis(compositions)
+        compositions, args = filter_compositions(request)
+        args.update(cluster_durations_frequency.analysis(compositions))
         return render(request, 'cluster_durations_frequency_result.html', args)
 
-    args = {'compositions': uniq_items_in_model('title', Composition),
-            'keys': uniq_items_in_model('key'),
-            'durations': uniq_items_in_model('total_duration'),
-            'signatures': uniq_items_in_model('time_signature'),
-    }
+    args = make_filter_args(Composition)
     return render(request, 'cluster_durations_frequency.html', args)
 
 
 def show_cluster_contour(request):
     if request.method == 'POST':
-        kwargs = {}
-
-        title = request.POST['select-composition']
-        key = request.POST['select-key']
-        total_duration = request.POST['select-duration']
-        time_signature = request.POST['select-time-signature']
         contour_size = request.POST['select-contour-size']
-
-        select_filter('title__iexact', title, kwargs, template='%s')
-        select_filter('key', key, kwargs)
-        select_filter('total_duration', total_duration, kwargs)
-        select_filter('time_signature', time_signature, kwargs)
-
-        compositions = Composition.objects.filter(**kwargs)
-        args = cluster_contour.analysis(compositions, int(contour_size))
+        compositions, args = filter_compositions(request)
+        args.update(cluster_contour.analysis(compositions, int(contour_size)))
         return render(request, 'cluster_contour_result.html', args)
 
-    args = {'compositions': uniq_items_in_model('title', Composition),
-            'keys': uniq_items_in_model('key'),
-            'durations': uniq_items_in_model('total_duration'),
-            'signatures': uniq_items_in_model('time_signature'),
-            'size_numbers': range(2, 5)
-    }
-    return render(request, 'cluster_contour.html', args)
+    args = make_filter_args(Composition)
+    args['size_numbers'] = range(2, 5)
+    return render(request, 'cluster_contour.html', args)    
 
 
 def show_cluster_all(request):
     if request.method == 'POST':
-        kwargs = {}
-
-        title = request.POST['select-composition']
-        key = request.POST['select-key']
-        total_duration = request.POST['select-duration']
-        time_signature = request.POST['select-time-signature']
         contour_size = request.POST['select-contour-size']
-
-        select_filter('title__iexact', title, kwargs, template='%s')
-        select_filter('key', key, kwargs)
-        select_filter('total_duration', total_duration, kwargs)
-        select_filter('time_signature', time_signature, kwargs)
-
-        compositions = Composition.objects.filter(**kwargs)
-        args = cluster_all.analysis(compositions, int(contour_size))
+        compositions, args = filter_compositions(request)
+        args.update(cluster_all.analysis(compositions, int(contour_size)))
         return render(request, 'cluster_all_result.html', args)
 
-    args = {'compositions': uniq_items_in_model('title', Composition),
-            'keys': uniq_items_in_model('key'),
-            'durations': uniq_items_in_model('total_duration'),
-            'signatures': uniq_items_in_model('time_signature'),
-            'size_numbers': range(2, 5)
-    }
-    return render(request, 'cluster_all.html', args)
+    args = make_filter_args(Composition)
+    args['size_numbers'] = range(2, 5)
+    return render(request, 'cluster_all.html', args)   
 
 
 def stats(request):
@@ -388,6 +258,7 @@ def stats(request):
         'number_compositions': Composition.objects.count(),
     }
     return render(request, 'stats.html', args)
+
 
 def show_main_cluster(request):
     return render(request, 'cluster.html', {})
