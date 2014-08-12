ac2a054d19b392ec95f1b2504b1358f40cf353a3
Marcos da Silva Sampaio <marcos@sampaio.me>
2014-08-12 09:43:50 -0300

    Remove unnecessary functions

--- analysis/computation/intervals.py
+++ analysis/computation/intervals.py
@@ -15,22 +15,8 @@ def frequency_scatter(intervals):
     return seq
 
 
-def frequency_basic_scatter(intervals):
-    simple_intervals = [abs(x) % 12 for x in intervals]
-    counted = utils.special_counter(simple_intervals, True)
-    seq = sorted(map(list, counted.items()))
-    seq.insert(0, ['Interval', 'Amount'])
-    return seq
-
-
 def frequency_pie(intervals):
     r = utils.aux_pie_chart(utils.special_counter(intervals))
-    r.insert(0, ['Interval', 'Amount'])
-    return r
-
-
-def chromatic_frequency_pie(chromatic_intervals):
-    r = utils.aux_pie_chart(utils.special_counter(chromatic_intervals, False))
     r.insert(0, ['Interval', 'Amount'])
     return r
 
@@ -46,16 +32,17 @@ def chromatic_leaps_frequency_pie(chromatic_intervals):
 
 def analysis(compositions):
     midi_intervals = utils.get_music_data_attrib(compositions, 'intervals_midi')
+    simple_intervals = [abs(x) % 12 for x in midi_intervals]
     chromatic_intervals = utils.get_music_data_attrib(compositions, 'intervals')
     basic_stats = utils.aux_basic_stats(midi_intervals, 'Intervals number', False)
 
     if midi_intervals and chromatic_intervals:
         args = {
             'frequency_scatter': frequency_scatter(midi_intervals),
-            'frequency_basic_scatter': frequency_basic_scatter(midi_intervals),
+            'frequency_basic_scatter': frequency_scatter(simple_intervals),
             'basic_stats': basic_stats,
             'frequency_pie': frequency_pie(midi_intervals),
-            'chromatic_frequency_pie': chromatic_frequency_pie(chromatic_intervals),
+            'chromatic_frequency_pie': frequency_pie(chromatic_intervals),
             'chromatic_leaps_frequency_pie': chromatic_leaps_frequency_pie(chromatic_intervals),
             'histogram': utils.histogram(midi_intervals, 10, ['Intervals', 'Ocurrences'], False, True),
             'distribution_amount': utils.distribution(midi_intervals, basic_stats, True),
