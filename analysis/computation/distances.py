5802d8e9f400d360a4c31c10550c9ec1db3f6a04
Marcos da Silva Sampaio <marcos@sampaio.me>
2014-08-14 09:56:15 -0300

    Add durations and contour in distances analysis

--- analysis/computation/distances.py
+++ analysis/computation/distances.py
@@ -1,5 +1,6 @@
 from analysis.computation import utils
 from analysis.computation import intervals
+from analysis.computation import contour
 
 
 def analysis(compositions):
@@ -8,16 +9,42 @@ def analysis(compositions):
     midi_intervals = utils.get_music_data_attrib(compositions, 'intervals_midi', 'extend')
     nested_midi_intervals = utils.get_music_data_attrib(compositions, 'intervals_midi', 'append')
 
+    # durations
+    dur = utils.get_music_data_attrib(compositions, 'durations', 'extend')
+    nested_dur = utils.get_music_data_attrib(compositions, 'durations', 'append')
 
-    if midi_intervals and nested_midi_intervals:
+    # contour
+    contour_list = utils.get_music_data_attrib(compositions, 'contour', 'append')
+
+    tests = [midi_intervals,
+             nested_midi_intervals,
+             dur,
+             nested_dur,
+             contour_list]
+
+    if all(tests):
         # intervals
-        categorized_midi_intervals = list(map(intervals.get_category, midi_intervals))
-        categorized_nested_midi_intervals = [list(map(intervals.get_category, s)) for s in nested_midi_intervals]
-        coll_freq_dic = utils.special_counter(categorized_midi_intervals, True)
+        cat_intervals = list(map(intervals.get_category, midi_intervals))
+        cat_nested_intervals = [list(map(intervals.get_category, s)) for s in nested_midi_intervals]
+        coll_freq_intervals = utils.special_counter(cat_intervals, True)
+
+        # durations
+        coll_freq_durations = utils.special_counter(dur, True)
+
+        # contour
+        freq_dist_args = {}
+        for size in range(2, 5):
+            split_seq = list(map(tuple, contour.split_and_translate_nested(contour_list, size)))
+            split_seq_nested = [list(map(tuple, contour.split_and_translate(c, size))) for c in contour_list]
+            coll_freq_dic = contour.get_frequency(split_seq, size)
+            freq_dist_sc = utils.frequency_distance_scatter(compositions, split_seq_nested, coll_freq_dic)
+            freq_dist_args.update({'contour_{}_distance'.format(size): freq_dist_sc})
 
         args = {
-            'intervals_distance': utils.frequency_distance_scatter(compositions, categorized_nested_midi_intervals, coll_freq_dic),
+            'intervals_distance': utils.frequency_distance_scatter(compositions, cat_nested_intervals, coll_freq_intervals),
+            'durations_distance': utils.frequency_distance_scatter(compositions, nested_dur, coll_freq_durations),
         }
+        args.update(freq_dist_args)
     else:
         args = {}
 
